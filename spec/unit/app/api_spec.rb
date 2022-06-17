39eb010579badc2d0c07137fbeac09ca197d3894
gabflima <gabriel.ferrugem.lima@gmail.com>
2022-06-16 23:21:30 -0300

    Added example for return 200 when successfully record expense.

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -34,7 +34,15 @@ module ExpenseTracker
           expect(parsed).to include('expense_id' => 417)
         end
 
-        it 'responds with a 200 (ok)'
+        it 'responds with a 200 (ok)' do
+          expense = {'some' => 'data'}
+          allow(ledger).to receive(:record)
+          .with(expense)
+          .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
       end
 
       context 'when the expense fails validation' do
