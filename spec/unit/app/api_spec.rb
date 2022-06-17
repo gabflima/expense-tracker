35f3973e04296f9de6c427d4ae1b273b94cde242
gabflima <gabriel.ferrugem.lima@gmail.com>
2022-06-17 18:25:11 -0300

    Refactoring.

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -10,7 +10,7 @@ module ExpenseTracker
   RSpec.describe API do
     include Rack::Test::Methods
 
-    # declare a double
+    # declare a double, which will replace every call to the class in argument
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
     def app
@@ -18,28 +18,25 @@ module ExpenseTracker
     end
 
     describe 'POST /expenses' do
+      let(:expense) { 
+        { 'some' => 'data' }
+      }
+
+      before do
+        # declare a mapping of function call and argument to response in double
+        allow(ledger).to receive(:record)
+          .with(expense)
+          .and_return(RecordResult.new(true, 417, nil))
+      end
+      
       context 'when the expense is sucessfully recorded' do
         it 'returns the expense id' do
-          expense = { 'some' => 'data' }
-
-          # declare a mapping of function call and argument to response
-          # in double
-          allow(ledger).to receive(:record)
-            .with(expense)
-            .and_return(RecordResult.new(true, 417, nil))
-
           post '/expenses', JSON.generate(expense)
-
           parsed = JSON.parse(last_response.body)
           expect(parsed).to include('expense_id' => 417)
         end
 
         it 'responds with a 200 (ok)' do
-          expense = {'some' => 'data'}
-          allow(ledger).to receive(:record)
-          .with(expense)
-          .and_return(RecordResult.new(true, 417, nil))
-
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(200)
         end
