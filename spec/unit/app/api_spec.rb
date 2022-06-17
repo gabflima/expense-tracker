25966160c2666a52caafae91b199a12d942784fd
gabflima <gabriel.ferrugem.lima@gmail.com>
2022-06-16 22:14:58 -0300

    Added Unit Test Specs for API's route POST /expenses, with future implementation of examples.

---
+++ spec/unit/app/api_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../../../app/api'
+
+module ExpenseTracker
+  RSpec.describe API do
+    describe 'POST /expenses' do
+      context 'when the expense is sucessfully recorded' do
+        it 'returns the expense id'
+        it 'responds with a 200 (ok)'
+      end
+
+      context 'when the expense fails validation' do
+        it 'returns an error message'
+        it 'responds with a 422 (Unprocessable entity)'
+      end
+    end
+  end
+end
