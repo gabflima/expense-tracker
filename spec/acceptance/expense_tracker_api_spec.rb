840db07038c2f4986dca4e7d27bcca6e7c317d6a
gabflima <gabriel.ferrugem.lima@gmail.com>
2022-06-16 21:38:27 -0300

    Refactor in ExpenseTracker Acceptance Test.

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -9,15 +9,8 @@ module ExpenseTracker
     def app
       ExpenseTracker::API.new
     end
-  
-    it 'records submitted expenses' do
-      # Arrange
-      coffee = {
-        'payee' => 'Starbucks',
-        'amount' => 5.75,
-        'date' => '2017-06-10'
-      }
-
+    
+    def post_expense(expense)
       # Act
       # Rack::Test::Methods#post - simulate posts by direct call, no parsing
       post '/expenses', JSON.generate(coffee)
@@ -26,6 +19,18 @@ module ExpenseTracker
       parsed = JSON.parse(last_response.body)
       expect(last_response.status).to eq(200)
       expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
+    it 'records submitted expenses' do
+      # Arrange
+      coffee = {
+        'payee' => 'Starbucks',
+        'amount' => 5.75,
+        'date' => '2017-06-10'
+      }
+
+      post_expense(coffee)
     end
   end
 end
